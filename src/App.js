0cf6612e6fb2b4664dfceb331da3321a869a9ee5
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-12-29 22:18:47 +0200

    removed the flash effect of unauthorized content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import { useEffect, Suspense, lazy } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Switch } from 'react-router-dom';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 import AppBar from 'components/AppBar/AppBar';
 import { fetchCurrentUser } from 'redux/auth/auth-operations';
+import { getIsFetchingCurrent } from 'redux/auth/auth-selectors';
 const HomeView = lazy(() => import('./views/HomeView'));
 const LoginView = lazy(() => import('views/LoginView'));
 const RegisterView = lazy(() => import('views/RegisterView'));
@@ -14,32 +15,40 @@ const ContactsView = lazy(() => import('views/ContactsView'));
 
 export default function App() {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(getIsFetchingCurrent);
 
   useEffect(() => {
     dispatch(fetchCurrentUser());
   }, [dispatch]);
 
   return (
-    <>
-      <AppBar />
-      <div>
-        <Switch>
-          <Suspense fallback={<p>Loading...</p>}>
-            <PublicRoute exact path="/">
-              <HomeView />
-            </PublicRoute>
-            <PublicRoute exact path="/login" redirectTo="/contacts" restricted>
-              <LoginView />
-            </PublicRoute>
-            <PublicRoute exact path="/register" restricted>
-              <RegisterView />
-            </PublicRoute>
-            <PrivateRoute path="/contacts" redirectTo="/login">
-              <ContactsView />
-            </PrivateRoute>
-          </Suspense>
-        </Switch>
-      </div>
-    </>
+    !isFetchingCurrentUser && (
+      <>
+        <AppBar />
+        <div>
+          <Switch>
+            <Suspense fallback={<p>Loading...</p>}>
+              <PublicRoute exact path="/">
+                <HomeView />
+              </PublicRoute>
+              <PublicRoute
+                exact
+                path="/login"
+                redirectTo="/contacts"
+                restricted
+              >
+                <LoginView />
+              </PublicRoute>
+              <PublicRoute exact path="/register" restricted>
+                <RegisterView />
+              </PublicRoute>
+              <PrivateRoute path="/contacts" redirectTo="/login">
+                <ContactsView />
+              </PrivateRoute>
+            </Suspense>
+          </Switch>
+        </div>
+      </>
+    )
   );
 }
--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -3,3 +3,5 @@ export const getIsloggedIn = state => state.auth.isLoggedIn;
 export const getUsername = state => state.auth.user.name;
 
 export const getUseremail = state => state.auth.user.email;
+
+export const getIsFetchingCurrent = state => state.auth.isFetchingCurrentUser;
