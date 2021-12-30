f21428dc94bc5827f8c453d659adbd86296c7684
Myroniuk Ilona <knopkakyrs@gmail.com>
2021-12-30 02:15:51 +0200

    add main image

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from './HomeView.module.css';
+import tree from '../img/27.png';
 
 const HomeView = () => (
   <div className={styles.container}>
     <h1 className={styles.title}>HELLO! CREARE YOUR PERSONAL PHONEBOOK!!!</h1>
+    <img className={styles.main_img} src={tree} alt="tree-icon" />
   </div>
 );
 
